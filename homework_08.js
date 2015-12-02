774d60002c0fd0f6b6de2f2cb62a3107457694d2
annaelena <nmariana@yahoo.it>
2015-12-02 08:53:53 +0100

    compiti

--- homework_08.js
+++ homework_08.js
@@ -1,4 +1,4 @@
-function ArrayList(){
+/*function ArrayList(){
     var array =[];
 
     this.insert = function(item){
@@ -41,4 +41,33 @@ function ArrayList(){
             result.push(right[j++]);
         }
         return result;
+    }*/
+
+function merge(ar1, ar2) { 
+     var result = []; 
+     var i1 = 0; 
+     var i2 = 0; 
+ 
+ 
+     while(i1 < ar1.length && i2 < ar2.length) { 
+         if(ar1[i1] < ar2[i2]) { 
+             result.push(ar1[i1]); 
+             i1++; 
+         } else { 
+             result.push(ar2[i2]); 
+             i2++; 
+         } 
+     } 
+     return result.concat(ar1.slice(i1)).concat(ar2.slice(i2)) 
+ } 
+
+function mergesort(myarray){
+    if (myarray.length == 1) {
+        return myarray;
     }
+    var mid = Math.ceil(myarray.length /2);
+    return merge(mergesort(myarray.slice(0,mid)), mergesort(myarray.slice(mid)));
+    
+}
+
+ 
